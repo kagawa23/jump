92bd025a78aef33c23347743243864a5c41945bb
Zhaochen <zhaochen01@juran.com.cn>
2018-05-25 18:35:23 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -9,6 +9,27 @@ var score = document.querySelector('.score')
 
 restartButton.addEventListener('click', restart)
 
+var maxscore = -1;
+
+//
+var nebpay = require('nebPay');
+var to = this.dappAddress;
+var maxScore = 0;
+var maxAddress = "";
+var callFunction = "getMaxScore";
+
+var callArgs = JSON.stringify([""])
+
+nebPay.simulateCall(to, value, callFunction, callArgs, {    
+  listener: function(resp){ 
+      var result = resp.result;
+      var resultObj = JSON.parse(result);
+      if(result.err) return;
+      maxscore = resultObj.maxScore;
+      maxAddress = resultObj.address;
+  }
+});
+
 // 游戏重新开始，执行函数
 function restart() {
   mask.style.display = 'none'
